66b455cb92a6eca21776d7966da1348a6103c18b
ThomasMeyers <thomas.meyers@polytechnique.edu>
2016-06-19 11:59:40 +0200

    Adding projet

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('frontProjetScola', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'restangular', 'ui.router', 'ngMaterial', 'toastr']);
+
+})();
